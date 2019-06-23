62df7b4b42b9986bb46b0709a65d673004083456
versotech <48041831+versotech@users.noreply.github.com>
2019-06-22 20:28:50 -0700

    Add files via upload

---
+++ l10n_mx_edi_addendas/tests/__init__.py
@@ -0,0 +1,20 @@
+# See LICENSE file for full copyright and licensing details.
+
+from . import test_addenda_aam
+from . import test_addenda_agnico
+from . import test_addenda_ahmsa
+from . import test_addenda_edumex
+from . import test_addenda_envases
+from . import test_addenda_ford
+from . import test_addenda_nestle
+from . import test_addenda_flextronics
+from . import test_addenda_nissan
+from . import test_addenda_sanmina
+from . import test_addenda_sidel
+from . import test_addenda_mabe
+from . import test_addenda_femsa
+from . import test_addenda_calsonickansei
+from . import test_addenda_pepsico
+from . import test_addenda_encinas
+from . import test_addenda_vallen
+from . import test_addenda_zfmexico
---
+++ l10n_mx_edi_addendas/tests/test_addenda_agnico.py
@@ -0,0 +1,25 @@
+# See LICENSE file for full copyright and licensing details.
+
+from .common import AddendasTransactionCase
+
+
+class TestAddendaAgnico(AddendasTransactionCase):
+
+    def setUp(self):
+        super(TestAddendaAgnico, self).setUp()
+        self.install_addenda('agnico')
+
+    def test_001_addenda_agnico(self):
+        invoice = self.create_invoice()
+        invoice.name = '0131'
+        invoice.action_invoice_open()
+        invoice.refresh()
+        self.assertEqual(invoice.state, "open")
+        self.assertEqual(invoice.l10n_mx_edi_pac_status, "signed",
+                         invoice.message_ids.mapped('body'))
+
+        # Check addenda has been appended and it's equal to the expected one
+        xml = invoice.l10n_mx_edi_get_xml_etree()
+        self.assertTrue(hasattr(xml, 'Addenda'), "There is no Addenda node")
+        expected_addenda = self.get_expected_addenda('agnico')
+        self.assertEqualXML(xml.Addenda, expected_addenda)
