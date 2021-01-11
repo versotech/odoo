cc68881ca68c610e83d8be1f320c9d5db9b77140
versotech <48041831+versotech@users.noreply.github.com>
2021-01-11 15:06:27 -0600

    Add files via upload

---
+++ l10n_mx_edi_customer_bills/models/account_payment.py
@@ -0,0 +1,12 @@
+from odoo import api, models
+
+
+class AccountPayment(models.Model):
+    _inherit = 'account.payment'
+
+    @api.multi
+    def l10n_mx_edi_is_required(self):
+        get_param = self.env['ir.config_parameter'].sudo().get_param
+        if get_param('l10n_mx_edi.avoid_stamp_payments'):
+            return False
+        return super(AccountPayment, self).l10n_mx_edi_is_required()
